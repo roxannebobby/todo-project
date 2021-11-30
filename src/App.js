47656120b03db5577d0eae8dfff60e8d7d572cf1
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-30 15:30:23 -0600

    added list framework to display all items

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ function App() {
 	const [isEditing, setIsEditing] = useState(false);
 	const [editID, setEditID] = useState(null);
 	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('hello');
+		if (!name) {
+			// display alert
+		} else if (name && isEditing) {
+			// deal with edit
+		} else {
+			// show alert
+			const newItem = { id: new Date().getTime().toString(), title: name };
+			setList([...list, newItem]);
+			setName('');
+		}
 	};
 	return (
 		<section className='section-center'>
@@ -31,7 +41,7 @@ function App() {
 				</div>
 			</form>
 			<div className='grocery-container'>
-				<List />
+				<List items={list} />
 				<button className='clear-btn'>clear items</button>
 			</div>
 		</section>
--- src/List.js
+++ src/List.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
-const List = () => {
-	return <></>;
+const List = ({ items }) => {
+	return (
+		<>
+			<div className='grocery-list'>
+				{items.map((item) => {
+					const { id, title } = item;
+					return (
+						<article key={id} className='grocery-item'>
+							<p className='title'>{title}</p>
+							<div className='btn-container'>
+								<button className='edit-btn' type='button'>
+									<FaEdit />
+								</button>
+								<button className='delete-btn' type='button'>
+									<FaTrash />
+								</button>
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</>
+	);
 };
 
 export default List;
